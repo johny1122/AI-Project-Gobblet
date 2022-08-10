726efdb2d59da017c751d873068490af17026313
Jonathan Birnbaum <yonatanb95@gmail.com>
2022-08-10 14:21:49 +0300

    add more methods to board

--- Board.py
+++ Board.py
@@ -3,31 +3,25 @@ from cell import Cell
 from piece import Piece
 from pieces_stack import PiecesStack
 from location import Location
+from action import Action
 from globals import *
 
 
 class Board:
-    rows = [[Location(i, j) for j in range(ROW_COL_LENGTH)] for i in range(ROW_COL_LENGTH)]
-    columns = [[Location(i, j) for i in range(ROW_COL_LENGTH)] for j in range(ROW_COL_LENGTH)]
     left_diagonal = [Location(i, i) for i in range(ROW_COL_LENGTH)]
     right_diagonal = [Location(i, 2 - i) for i in range(ROW_COL_LENGTH)]
+    rows = [[Location(i, j) for j in range(ROW_COL_LENGTH)] for i in range(ROW_COL_LENGTH)]
+    columns = [[Location(i, j) for i in range(ROW_COL_LENGTH)] for j in range(ROW_COL_LENGTH)]
     lines = [left_diagonal, right_diagonal] + rows + columns
 
     def __init__(self):
-        self.whites = [Piece(SMALL, WHITE), Piece(MEDIUM, WHITE), Piece(LARGE, WHITE)] * 3
-        self.blacks = [Piece(SMALL, BLACK), Piece(MEDIUM, BLACK), Piece(LARGE, BLACK)] * 3
-
-        self.stacks_white = [
-            PiecesStack(self.whites[:3]),
-            PiecesStack(self.whites[3:6]),
-            PiecesStack(self.whites[6:])
-        ]
-
-        self.stacks_black = [
-            PiecesStack(self.blacks[:3]),
-            PiecesStack(self.blacks[3:6]),
-            PiecesStack(self.blacks[6:])
-        ]
+        self.stacks = {color: [] for color in COLORS}
+        for color in COLORS:
+            for stack_index in range(STACKS_NUM):
+                stack = PiecesStack()
+                for size in SIZES:
+                    stack.add(Piece(size, color, stack_index))
+                self.stacks[color].append(stack)
 
         self.cells = []
         for row_index in range(ROW_COL_LENGTH):
@@ -41,9 +35,9 @@ class Board:
 
     def get_possible_pieces_outside(self, color: str) -> List[Piece]:
         if color == WHITE:
-            stacks = self.stacks_white
-        else:
-            stacks = self.stacks_black
+            stacks = self.stacks[WHITE]
+        elif color == BLACK:
+            stacks = self.stacks[BLACK]
 
         possible_outsides = []
         for stack in stacks:
@@ -60,7 +54,7 @@ class Board:
 
         return inside_pieces
 
-    def get_possible_pieces(self, color: str) -> List[Piece]:
+    def get_available_pieces(self, color: str) -> List[Piece]:
         return self.get_possible_pieces_outside(color) + self.get_possible_pieces_inside(color)
 
     def count_colors_in_line(self, line: List[Location]) -> Tuple[int, int]:
@@ -83,3 +77,70 @@ class Board:
                 return True, BLACK, line
 
         return False
+
+    # TODO: maybe add to Cell for not calculating each time
+    def lines_of_cell(self, cell: Cell) -> List[List[Location]]:
+        lines = [line for line in self.lines if cell.location in line]
+        return lines
+
+    def is_action_legal(self, action: Action) -> bool:
+        src_cell = self.get_cell(action.src)
+        dest_cell = self.get_cell(action.dest)
+        piece_to_move = action.piece
+
+        if dest_cell.is_empty():  # dest is empty
+            return True
+
+        # dest is not empty
+        if dest_cell.top().size < piece_to_move.size:  # dest size smaller than piece_to_move
+            if not src_cell.location.is_outside():  # src is inside board
+                return True
+
+            else:  # src is outside board
+                # check there are 2 opponent pieces in one of dest_cell's lines
+                lines_of_dest_cell = self.lines_of_cell(dest_cell)
+                for line in lines_of_dest_cell:
+                    whites, blacks = self.count_colors_in_line(line)
+                    if piece_to_move.color == WHITE:
+                        if blacks == ROW_COL_LENGTH - 1:
+                            return True
+                    elif piece_to_move.color == BLACK:
+                        if whites == ROW_COL_LENGTH - 1:
+                            return True
+        return False
+
+    def get_actions_of_piece(self, piece: Piece) -> List[Action]:
+        possible_actions = []
+        for row in self.cells:
+            for cell in row:
+                new_action = Action(piece, piece.location, cell.location)
+                if self.is_action_legal(new_action):
+                    possible_actions.append(new_action)
+
+        return possible_actions
+
+    def get_legal_actions(self, color: str) -> List[Action]:
+        all_possible_pieces = self.get_available_pieces(color)
+        legal_actions = []
+        for piece in all_possible_pieces:
+            legal_actions.extend(self.get_actions_of_piece(piece))
+
+        return legal_actions
+
+    def apply_action(self, action: Action) -> None:
+        piece = action.piece
+        src = action.src
+        dest = action.dest
+
+        # add piece to new location
+        self.cells[dest.row][dest.col].add(piece)
+        self.cells[dest.row][dest.col].top().location = dest
+
+        # remove piece from src stack
+        if src.is_outside():
+            self.cells[dest.row][dest.col].top().stack_index = NONE
+            self.stacks[piece.color][piece.stack_index].pop()
+
+        # remove piece from src cell
+        else:
+            self.cells[src.row][src.col].pop()
--- piece.py
+++ piece.py
@@ -3,7 +3,8 @@ from globals import *
 
 
 class Piece:
-    def __init__(self, size: int, color: str):
+    def __init__(self, size: int, color: str, stack_index: int = NONE):
         self.size = size
         self.color = color
         self.location = Location(row=OUTSIDE, col=OUTSIDE)
+        self.stack_index = stack_index
